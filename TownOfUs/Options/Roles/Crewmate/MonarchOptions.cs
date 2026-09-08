f41568498a93c82d86e6f7ee6610bb7d3cc925a6
Fault <26510624+soyFault@users.noreply.github.com>
2026-09-08 12:51:59 -0600

    Update MonarchOptions.cs

--- TownOfUs/Options/Roles/Crewmate/MonarchOptions.cs
+++ TownOfUs/Options/Roles/Crewmate/MonarchOptions.cs
@@ -10,28 +10,28 @@ public sealed class MonarchOptions : AbstractRoleOptionGroup<MonarchRole>
 {
     public override string GroupName => MiraLocaleManager.Get("TownOfUsMira.Role.Monarch", "Monarch");
 
-    [ModdedNumberOption("Knight Cooldown", 5f, 30f, 2.5f, MiraNumberSuffixes.Seconds)]
+    [ModdedNumberOption("Recarga de Investir", 5f, 30f, 2.5f, MiraNumberSuffixes.Seconds)]
     public float KnightCooldown { get; set; } = 20f;
 
-    [ModdedNumberOption("Maximum Knights", 0f, 15f, 1f, MiraNumberSuffixes.None, "0", zeroInfinity: true)]
+    [ModdedNumberOption("Máx Caballeros", 0f, 15f, 1f, MiraNumberSuffixes.None, "0", zeroInfinity: true)]
     public float MaxKnights { get; set; } = 3f;
-    [ModdedNumberOption("Votes Per Knight", 1f, 5f, 1f, MiraNumberSuffixes.None, "0")]
+    [ModdedNumberOption("Votos por Caballero", 1f, 5f, 1f, MiraNumberSuffixes.None, "0")]
     public float VotesPerKnight { get; set; } = 1f;
-    [ModdedNumberOption("Knight Delay (Cancellable)", 1f, 10f, 1f, MiraNumberSuffixes.Seconds)]
+    [ModdedNumberOption("Retraso de Investir (Cancelable)", 1f, 10f, 1f, MiraNumberSuffixes.Seconds)]
     public float KnightDelay { get; set; } = 3f;
 
-    [ModdedToggleOption("Reveal Knighting At Meeting")]
+    [ModdedToggleOption("Reveal Caballeros en Reunión")]
     public bool RevealAtMeeting { get; set; } = false;
 
-    [ModdedToggleOption("Show Knighted Votes")]
+    [ModdedToggleOption("Mostrar Votos de Caballero")]
     public bool ShowKnightedVotes { get; set; } = true;
 
-    [ModdedToggleOption("Allow Round One Knighting")]
+    [ModdedToggleOption("Permitir en Ronda 1")]
     public bool FirstRoundUse { get; set; } = false;
-    [ModdedToggleOption("Inform Monarch If a Knight Dies")]
+    [ModdedToggleOption("Avisar si muere un Caballero")]
     public bool InformWhenKnightDies { get; set; } = true;
 
-    [ModdedToggleOption("Crew Knights Grant Kill Immunity")]
+    [ModdedToggleOption("Caballeros Tripulantes dan Inmunidad")]
     public bool CrewKnightsGrantKillImmunity { get; set; } = true;
 }
 
